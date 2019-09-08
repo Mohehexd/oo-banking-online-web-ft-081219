0ca023e2ec60b73e9ea707d0a38199cb56e2e285
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-08 00:59:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    # binding.pry 
+    binding.pry 
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
