a78d0781e288f00428ccee0f1502eb2bca898e83
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-08 00:30:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,32 +1,66 @@
-require "pry"
-class Transfer
+# require "pry"
+# class Transfer
 
-  attr_accessor :sender, :receiver, :status, :amount
+#   attr_accessor :sender, :receiver, :status, :amount
   
+#   def initialize(sender, receiver, amount)
+#     @sender = sender 
+#     @receiver = receiver
+#     @amount = amount 
+#     @status = "pending" 
+#   end 
+  
+#   def valid? 
+#     @sender.valid? && @receiver.valid? 
+#   end 
+  
+#   def execute_transaction
+#     if @sender.balance > @amount && @status == "pending"
+#       @sender.balance -= @amount
+#       @receiver.balance += @amount
+#       @status = "complete"
+#     else
+#       @status = "closed"
+#       "Transaction rejected. Please check your account balance."
+#       # binding.pry 
+#     end
+#   end
+  
+#     def reverse_transfer
+#     if @status == "complete"
+#       @sender.balance += @amount
+#       @receiver.balance -= @amount
+#       @status = "reversed"
+#     end
+#   end
+# end
+
+class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
   def initialize(sender, receiver, amount)
-    @sender = sender 
+    @sender = sender
     @receiver = receiver
-    @amount = amount 
-    @status = "pending" 
-  end 
-  
-  def valid? 
-    @sender.valid? && @receiver.valid? 
-  end 
-  
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "closed"
-      "Transaction rejected. Please check your account balance."
-      # binding.pry 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
-  
-    def reverse_transfer
+
+  def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
@@ -34,7 +68,3 @@ class Transfer
     end
   end
 end
-
-# amanda.balance).to eq(950)
-#       expect(avi.balance).to eq(1050)
-#       expect(transfer.status
